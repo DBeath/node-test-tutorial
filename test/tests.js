8196d53aeda4dab5e2465ffa1acdd0721e7f7f57
David Beath <davidgbeath@gmail.com>
2014-03-04 12:44:23 +1300

    Converted indentation to spaces

--- test/tests.js
+++ test/tests.js
@@ -5,56 +5,56 @@ var expect = require('chai').expect;
 var request = require('request');
 
 describe('response', function() {
-	before(function () {
-		server.listen(8000);
-	});
+  before(function () {
+    server.listen(8000);
+  });
 
-	after(function () {
-		server.close();
-	});
+  after(function () {
+    server.close();
+  });
 
-	it('should return 400', function (done) {
-		request.get('http://localhost:8000', function (err, res, body){
-			expect(res.statusCode).to.equal(400);
-			expect(res.body).to.equal('wrong header');
-			done();
-		});
-	});
+  it('should return 400', function (done) {
+    request.get('http://localhost:8000', function (err, res, body){
+      expect(res.statusCode).to.equal(400);
+      expect(res.body).to.equal('wrong header');
+      done();
+    });
+  });
 
-	it('should return 200', function (done) {
-		var options = {
-			url: 'http://localhost:8000',
-			headers: {
-				'Content-Type': 'text/plain'
-			}
-		};
-		request.get(options, function (err, res, body) {
-			expect(res.statusCode).to.equal(200);
-			expect(res.body).to.equal('correct header');
-			done();
-		});
-	});
+  it('should return 200', function (done) {
+    var options = {
+      url: 'http://localhost:8000',
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    };
+    request.get(options, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.equal('correct header');
+      done();
+    });
+  });
 
-	it('should emit request body', function (done) {
-		var options = {
-			url: 'http://localhost:8000',
-			headers: {
-				'Content-Type': 'text/plain'
-			},
-			body: 'successfully emitted request'
-		};
-		var eventFired = false;
+  it('should emit request body', function (done) {
+    var options = {
+      url: 'http://localhost:8000',
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'successfully emitted request'
+    };
+    var eventFired = false;
 
-		request.get(options, function (err, res, body) {});
+    request.get(options, function (err, res, body) {});
 
-		server.on('success', function (data) {
-			eventFired = true;
-			expect(data).to.equal('successfully emitted request');
-		});
+    server.on('success', function (data) {
+      eventFired = true;
+      expect(data).to.equal('successfully emitted request');
+    });
 
-		setTimeout( function () {
-			expect(eventFired).to.equal(true);
-			done();
-		}, 10);
-	});
+    setTimeout( function () {
+      expect(eventFired).to.equal(true);
+      done();
+    }, 10);
+  });
 });
